8b85b12919a71f50d279d9f7e3fa165f24162394
Zdeslav Vojkovic <zdeslav.vojkovic@gmail.com>
2013-05-20 00:00:26 +0200

    Dart linter cleanup

--- sublimelinter/modules/dart.py
+++ sublimelinter/modules/dart.py
@@ -3,7 +3,7 @@ from base_linter import BaseLinter, INPUT_METHOD_FILE
 
 
 CONFIG = {
-    'language': 'Dart',
+    'language': 'dart',
     'executable': 'dartanalyzer',
     'lint_args': ['--machine', '{filename}'],
     'test_existence_args': '--version',
@@ -14,21 +14,15 @@ CONFIG = {
 class Linter(BaseLinter):
     MESSAGE_RE = re.compile(r'^(?P<level>\w+)\|(?P<category>\w+)\|(?P<kind>\w+)\|(?P<path>.*)\|(?P<line>\d+)\|(?P<col>\d+)\|(?P<length>\d+)\|(?P<message>.*)')
 
-    def __init__(self, config):
-        super(Linter, self).__init__(config)
-        self.linter = None
-
     def parse_errors(self, view, errors, lines, errorUnderlines, violationUnderlines, warningUnderlines, errorMessages, violationMessages, warningMessages):
 
         for line in errors.splitlines():
             match = self.MESSAGE_RE.match(line)
             if match:
                 level = match.group('level')
-                cat = match.group('category')
-                kind = match.group('kind')
-                print("PROCESSING: {0} | {1} | {2}".format(level, cat, kind))
+                # cat = match.group('category')
+                # kind = match.group('kind')
                 line, col, span = match.group('line'), match.group('col'), match.group('length')
-                print("ERR: {0} {1}".format(line, level))
                 lineno = int(line)
                 colno = int(col) - 2
                 length = int(span)
@@ -46,5 +40,3 @@ class Linter(BaseLinter):
 
                 self.add_message(lineno, lines, message, messages)
                 self.underline_range(view, lineno, colno, underlines, length)
-            else:
-                print(" -- no match")
